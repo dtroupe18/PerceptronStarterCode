519af5a8fc439285f89b6fefe57007871256a481
Dave <Dave.Troupe@gmail.com>
2017-05-11 14:30:31 -0400

    updates

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -3,6 +3,30 @@
  */
 public class Perceptron {
 
-    /*
-     * Your Perceptron should be an object with methods to train and predict */
+    double[] weights = new double[4];
+    double[][] data;
+    double learningRate;
+    int iterations;
+
+    Perceptron(double learningRate, int iterations, double[][] data) {
+        this.learningRate = learningRate;
+        this.iterations = iterations;
+        this.data = data;
+    }
+
+    /* Your Perceptron should be an object with methods to train and predict */
+
+    public void train() {
+
+    }
+
+    public void predict() {
+
+    }
+
+    public void calculateAccuracy() {
+
+    }
+
+
 }
